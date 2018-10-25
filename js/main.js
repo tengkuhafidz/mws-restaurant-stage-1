890b3939cf6b74c9712e9e314e0ed4261b0fd48e
Tengku Hafidz <tengkuhafidz@gmail.com>
2018-10-26 02:05:33 +0800

    implement responsive styling

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoidGVuZ2t1aGFmaWR6IiwiYSI6ImNqbm9meGd3cTA1MXEzdm0xanM3emNoc3AifQ.ZoCPqBXtp35R6yJffbFiFQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
