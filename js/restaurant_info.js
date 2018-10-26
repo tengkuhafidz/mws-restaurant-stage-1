9322a7200991778269a153ce212ccd6bc7a35961
Tengku Hafidz <tengkuhafidz@gmail.com>
2018-10-26 18:52:36 +0800

    fix typo

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -90,7 +90,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
     const image = document.getElementById('restaurant-img');
     image.className = 'restaurant-img'
-    imagr.alt = restaurant.name + 'restaurant';
+    image.alt = restaurant.name + 'restaurant';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
     const cuisine = document.getElementById('restaurant-cuisine');
